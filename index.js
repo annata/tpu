7bf4a7ce40e3ec0becdd509fcb61b474390f87d8
wqy <annata@users.noreply.github.com>
2024-05-22 15:41:53 +0800

    fix tpu client

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {TpuConnection} from "tpu-client";
-import {Connection, Transaction} from "@solana/web3.js";
+import {Connection, Transaction, VersionedMessage, VersionedTransaction} from "@solana/web3.js";
 import express from "express";
-import base58 from "bs58";
+import bs58 from "bs58";
 
 let url = process.env.url;
 if (!url) {
@@ -25,39 +25,52 @@ app.use(express.json())
 app.post('/' + prefix, async (req, res) => {
     try {
         const param = req.body;
-        if (!"sendTransaction" === param.method) {
+        if (!"sendTransaction" === param?.method) {
             throw new Error("error method");
         }
-        const tx = param.params[0];
+        const tx = param?.params[0];
         if (!tx) {
             throw new Error("error tx");
         }
-        const encode = param.params[1]?.encoding;
-        let transaction;
+        const encode = param?.params[1]?.encoding;
+        let transactionBuf;
         if ("base64" === encode) {
-            transaction = tx;
+            transactionBuf = new Buffer(tx, 'base64');
         } else {
-            transaction = Buffer.from(base58.decode(encode)).toString("base64");
+            transactionBuf = Buffer.from(bs58.decode(encode));
         }
-        const result = await send(transaction);
-        res.json({result, jsonrpc: '2.0', id: param.id})
+        const transaction = VersionedTransaction.deserialize(transactionBuf);
+        sendTwice(transactionBuf);
+        const txid = bs58.encode(transaction.signatures[0]);
+        const result = txid;
+        res.json({result, jsonrpc: '2.0', id: param?.id})
     } catch (e) {
         console.log(e.toString());
         res.json({error: e.toString(),})
     }
 });
 
-async function send(transaction) {
-    const txid = await tpuConnection.sendEncodedTransaction(transaction);
-    sendTwice(transaction);
-    return txid;
+async function sendTwice(rawTransaction) {
+    send(rawTransaction);
+    await sleep(2000);
+    send(rawTransaction);
+    await sleep(2000);
+    send(rawTransaction);
 }
 
-async function sendTwice(transaction) {
-    await sleep(2000);
-    tpuConnection.sendEncodedTransaction(transaction);
-    await sleep(2000);
-    tpuConnection.sendEncodedTransaction(transaction);
+async function send(rawTransaction) {
+    const tpu_addresses = await tpuConnection.tpuClient.leaderTpuService.leaderTpuSockets(tpuConnection.tpuClient.fanoutSlots);
+    tpu_addresses.forEach(tpu_address => {
+        tpuConnection.tpuClient.sendSocket.send(rawTransaction, parseInt(tpu_address.split(':')[1]), tpu_address.split(':')[0], (error) => {
+            if (!error) {
+                // console.log("success")
+            } else {
+                console.error(error);
+            }
+        });
+    });
+    // const txid = await tpuConnection.sendRawTransaction(transaction);
+    // sendTwice(transaction);
 }
 
 app.listen(11011)
